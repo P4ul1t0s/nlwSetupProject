07407ce4352e4bc010b75fda5821a646fcb42673
Paulo Mafra <pa057329@intelbras.com.br>
2023-01-17 17:24:14 -0300

    class #01 - prisma inicial setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,16 @@
 import Fastify from "fastify";
+import {PrismaClient} from '@prisma/client'
 
 const app = Fastify();
+const prisma = new PrismaClient()
 
-app.get('/', () => {
-    return 'Hello World!'
+app.get('/hello', async () => {
+    const habits = await prisma.habit.findMany()
+    return habits
 })
 
 app.listen({
     port: 3003
+}).then(() => {
+    console.log('HTTP Server Running!\nDefault port is: 3003')
 })
