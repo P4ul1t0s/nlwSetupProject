14b314cffb5f4e1dc11d8c766324f8870de48dbd
Paulo Mafra <pa057329@intelbras.com.br>
2023-01-19 17:00:53 -0300

    placeholder to HabitDay

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,8 +1,4 @@
-interface HabitDayProps{
-    completed: number
-}
-
-export function HabitDay(props: HabitDayProps){
+export function HabitDay(){
     return(
         <div className="w-10 h-10 bg-zinc-900 border border-zinc-800 rounded-lg"></div>
     )
