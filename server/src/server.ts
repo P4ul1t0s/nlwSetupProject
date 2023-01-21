5c407c67cc0d769ae191fc7238c3e85b8363603d
Paulo <paulomafra2004@gmail.com>
2023-01-21 00:54:32 -0300

    class #04 - getting summary data form db

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,15 @@
 import fastify from "fastify";
-import Cors from "@fastify/cors";
+import cors from '@fastify/cors';
 import { appRoutes } from "./routes";
 
 const app = fastify();
 
-app.register(Cors)
+app.register(cors)
 app.register(appRoutes)
 
 app.listen({
-    port: 3003
+    port: 3003,
+    host: '0.0.0.0'
 }).then(() => {
     console.log('HTTP Server Running!\nAccess: http://localhost:3003/')
 })
